de6fe715aba626e01d98dec6353e88b503f7f5a6
Adam Cohan <adam141d@gmail.com>
2024-05-29 17:59:59 -0700

    did everything for world athletics except multis

--- world-athletics-scraping.py
+++ world-athletics-scraping.py
@@ -50,22 +50,60 @@ EVENT_DICTS = {
 
 WA_URL = 'https://worldathletics.org'
 
-def scrape_event(event_url):
+def add_athlete(d, data, year):
+    name = data['Name']
+    place = data['Place']
+    mark = data['Mark']
+    country = data['Country']
+
+    yc = 'years_competed'
+    fp = 'finish_placements'
+    fm = 'finish_marks'
+
+    if name in d.keys():
+        athlete_dict = d[name]
+        athlete_dict[yc].append(year)
+        athlete_dict[fp].append(place)
+        athlete_dict[fm].append(mark)
+    else:
+        d[name] = {yc:[year], fp:[place], fm:[mark], 'country':country}
+
+def scrape_event(event_url, meet_year):
     page = requests.get(event_url)
     soup = BeautifulSoup(page.content, 'html.parser')
 
     athletes = soup.find('tbody').find_all('tr')
-
     list_of_rows = []
     for row in athletes:
         cells = row.find_all('td')
         cells = [i.get_text().strip() for i in cells]
         list_of_rows.append(cells)
-    
+
     df = pd.DataFrame(list_of_rows)
-    # print(df)
+    df.rename(columns={0:'Place', 1:'Bib', 2:'Name', 3:'Country', 4:'Mark'}, inplace=True)
+    df['Mark'] = df['Mark'].apply(lambda mark : mark.split()[0])
+
+    event_name = soup.find('h1').get_text().strip().split()
+    event_sex = 'Womens' if event_name[-1] == 'women' else 'Mens' # to append for event dict
+    event_name = event_name[:-1]
+    if 'Metres' in event_name:
+        metre_index = event_name.index('Metres')
+        event_name[metre_index] = 'Meters' # USA USA USA
+        if 'Steeplechase' in event_name or 'Hurdles' in event_name:
+            event_name.remove('Meters')
+    if 'Throw' in event_name:
+        event_name.remove('Throw')
+    if '10,000' in event_name:
+        tenk_index = event_name.index('10,000')
+        event_name[tenk_index] = '10000'
+
+    tfrrs_style_name = event_sex + '-' + '-'.join(event_name)
+
+    # add to the big event dictionary
+    for idx, row in df.iterrows():
+        add_athlete(EVENT_DICTS[tfrrs_style_name], row, meet_year)
+
     return df
-    # here should call a function that adds the df to the global event dfs (similar to how they are for ncaa)
 
 
 def scrape_meet(meet_url):
@@ -90,7 +128,8 @@ def scrape_meet(meet_url):
     page = requests.get(meet_url)
     soup = BeautifulSoup(page.content, 'html.parser')
 
-    meet_name = soup.find('h1').get_text().strip()
+    # meet_name = soup.find('h1').get_text().strip()
+    meet_year = soup.find('span', {'class': '_label date'}).get_text().strip()[-4:]
 
     tables = soup.find_all('tbody')
     finals = []
@@ -103,29 +142,36 @@ def scrape_meet(meet_url):
 
         tfrrs_style_name = wa_style_to_tfrrs(event_name_og, event_sex)
 
-        
         is_ncaa_event = tfrrs_style_name in EVENT_DICTS.keys()
         
         if is_ncaa_event:
-            print(tfrrs_style_name)
             results_cell = final.parent.find_all('td')[5]
             results_url = WA_URL + results_cell.find('a', href=True)['href']
-            # scrape_event(results_url)
+            scrape_event(results_url, meet_year)
 
-    return meet_name
-
+    
     # need to do multis separately
     # look for 1500/800 with round == decathlon/heptathlon
 
-all_champs_url = 'https://worldathletics.org/results/world-athletics-championships'
+    # write all event dicts to new folder of csvs
 
-page = requests.get(all_champs_url)
-soup = BeautifulSoup(page.content, 'html.parser')
+def main():
+    all_champs_url = 'https://worldathletics.org/results/world-athletics-championships'
 
-meetings = soup.find('tbody').find_all('a', href=True)
-meet_urls = [i['href'] for i in meetings]
+    page = requests.get(all_champs_url)
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-for meet in meet_urls:
-    url = WA_URL + meet
-    meet_name = scrape_meet(url)
-    break
+    meetings = soup.find('tbody').find_all('a', href=True)
+    meet_urls = [i['href'] for i in meetings]
+
+    for meet in meet_urls:
+        url = WA_URL + meet
+        # print(meet)
+        scrape_meet(url)
+        break
+
+    # scrape_event("https://worldathletics.org/results/world-athletics-championships/2023/world-athletics-championships-budapest-2023-7138987/men/1500-metres/final/result#resultheader")
+
+    print(EVENT_DICTS)
+
+main()
